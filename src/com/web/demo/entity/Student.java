6edff1bcd3a5bf668e12b11d5d686d45c072df6f
Coder <2515946609@qq.com>
2019-11-27 09:51:26 +0800

    学生实体类

---
+++ src/com/web/demo/entity/Student.java
@@ -0,0 +1,41 @@
+package com.web.demo.entity;
+
+public class Student {
+    private int stuNo;
+
+    public int getStuNo() {
+        return stuNo;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "stuno=" + stuNo +
+                ", stuName='" + stuName + '\'' +
+                ", classId=" + classId +
+                '}';
+    }
+
+    public void setStuNo(int stuno) {
+        this.stuNo = stuno;
+    }
+
+    public String getStuName() {
+        return stuName;
+    }
+
+    public void setStuName(String stuName) {
+        this.stuName = stuName;
+    }
+
+    public int getClassId() {
+        return classId;
+    }
+
+    public void setClassId(int classId) {
+        this.classId = classId;
+    }
+
+    private String stuName;
+    private int classId;
+}
