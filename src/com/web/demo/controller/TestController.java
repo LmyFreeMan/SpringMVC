2b62e24d5694fbcde343fd3a3af7b0377c90027a
Coder <2515946609@qq.com>
2019-11-28 17:02:38 +0800

    测试控制器

--- src/com/web/demo/controller/TestController.java
+++ src/com/web/demo/controller/TestController.java
@@ -1,5 +1,8 @@
 package com.web.demo.controller;
+
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+@Controller
 public class TestController {
     @RequestMapping("/test.do")
     public String test(){
