790212116d861c3e20f7869af7d63d869cadb27c
Coder <2515946609@qq.com>
2020-01-31 20:47:43 +0800

    JsonController

--- src/com/web/demo/controller/JsonController.java
+++ src/com/web/demo/controller/JsonController.java
@@ -4,12 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
 @Controller
 public class JsonController {
     @RequestMapping("/tojson")
