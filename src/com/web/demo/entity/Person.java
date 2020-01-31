deb02bf839fac966da8afc27a29b40294a71c71c
Coder <2515946609@qq.com>
2020-01-31 20:43:04 +0800

    entity-Person

---
+++ src/com/web/demo/entity/Person.java
@@ -0,0 +1,11 @@
+package com.web.demo.entity;
+
+import java.util.List;
+
+public class Person {
+    private String name;
+    private String  age;
+    private boolean sex;
+    private List<String> hobbies;
+
+}
