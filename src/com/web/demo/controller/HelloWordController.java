c6c50acc0d8d788923631978cd9316ff6cd31cd3
Coder <2515946609@qq.com>
2020-01-31 20:47:27 +0800

    HelloWorldController

--- src/com/web/demo/controller/HelloWordController.java
+++ src/com/web/demo/controller/HelloWordController.java
@@ -1,8 +1,13 @@
 package com.web.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
 @Controller
+
 public class HelloWordController {
+    @RequestMapping("/he31")
     public String index(){
         return "index";
     }
